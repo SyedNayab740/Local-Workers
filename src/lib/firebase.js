76225207556b882c0eff84bc08104f8f6277f4d1
Syed Nayab <syednayabr1109@gmail.com>
2026-04-10 16:58:42 +0530

    final firebase fix

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,10 +3,10 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOURS",
+  apiKey: "AIzaSyDHnp7oLzgS0l7W7gZg-6c-bD1_3z2RFCA",
   authDomain: "local-workers-b33d2.firebaseapp.com",
   projectId: "local-workers-b33d2",
-  storageBucket: "local-workers-b33d2.appspot.com",
+  storageBucket: "local-workers-b33d2.firebasestorage.app", // ✅ use this
   messagingSenderId: "768305995847",
   appId: "1:768305995847:web:e55e98b6d5535d81121162"
 };
